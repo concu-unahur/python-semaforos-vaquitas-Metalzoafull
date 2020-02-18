13d814811453dbbada3f36b682b6ecf4162406e3
metalzoafull <juannico36@live.com.ar>
2020-02-18 20:23:15 -0300

    correccion

--- vaquitas.py
+++ vaquitas.py
@@ -6,6 +6,8 @@ import threading
 inicioPuente = 10
 largoPuente = 20
 
+sema = threading.Semaphore(2)
+
 class Vaca(threading.Thread):
   def __init__(self):
     super().__init__()
@@ -22,13 +24,24 @@ class Vaca(threading.Thread):
   def run(self):
     while(True):
       self.avanzar()
+      self.esperar()
+  
+  def esperar(self):
+    global sema
+    if self.posicion == inicioPuente:
+      sema.acquire()
+    elif self.posicion == (inicioPuente + largoPuente):
+      sema.release()
+
+class Puente(threading.Thread):
+  def __init__
 
 vacas = []
-sema = []
+#sema = []
 for i in range(5):
   v = Vaca()
-  semaforo = threading.Semaphore(1)
-  sema.append(semaforo)
+  #semaforo = threading.Semaphore(1)
+  #sema.append(semaforo)
   vacas.append(v)
   v.start()
 
@@ -38,18 +51,21 @@ def cls():
 def dibujarPuente():
   print(' ' * inicioPuente + '=' * largoPuente)
 
+
 while(True):
   cls()
   print('Apretá Ctrl + C varias veces para salir...')
   print()
+  #semaPuente.acquire()
   dibujarPuente()
-  #for v in vacas:
-  #  v.dibujar()
-  while(inicioPuente != 0):
-    for i in range(len(vacas)):
-      vacas[i].dibujar()
-    inicioPuente =-1
+  for v in vacas:
+    v.dibujar()
+  #while(inicioPuente != 0):
+  #  for i in range(len(vacas)):
+  #    vacas[i].dibujar()
+  #  inicioPuente =-1
    
   dibujarPuente()
+
   
   time.sleep(0.2)
